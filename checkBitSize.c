8141c1aab647860928ad881236a37c49eea5b176
bus <git@huttraeger.de>
2020-03-11 00:36:47 +0100

    retab

--- checkBitSize.c
+++ checkBitSize.c
@@ -3,11 +3,10 @@
 
 int main(void)
 {
-	const size_t targetSize = TARGET_SIZE;
-	const char* const result[] = { "error", "success" };
-	const int success = !!(sizeof(size_t) == targetSize && sizeof(void*) == targetSize);
-	fprintf( (success == 1) ? stdout : stderr, "%s! compiled for %s. size_t = " SIZE_T_FORMAT_STRING " bits, void* = " SIZE_T_FORMAT_STRING " bits. " SIZE_T_FORMAT_STRING " bits expected.\n", result[success], PLATFORM, SIZE_T_CAST(8*sizeof(size_t)), SIZE_T_CAST(8*sizeof(void*)), SIZE_T_CAST(8*targetSize));
-	MSVC_DEBUG_PAUSE;
-	return (success == 1) ? EXIT_SUCCESS : EXIT_FAILURE;
+        const size_t targetSize = TARGET_SIZE;
+        const char* const result[] = { "error", "success" };
+        const int success = !!(sizeof(size_t) == targetSize && sizeof(void*) == targetSize);
+        fprintf( (success == 1) ? stdout : stderr, "%s! compiled for %s. size_t = " SIZE_T_FORMAT_STRING " bits, void* = " SIZE_T_FORMAT_STRING " bits. " SIZE_T_FORMAT_STRING " bits expected.\n", result[success], PLATFORM, SIZE_T_CAST(8*sizeof(size_t)), SIZE_T_CAST(8*sizeof(void*)), SIZE_T_CAST(8*targetSize));
+        MSVC_DEBUG_PAUSE;
+        return (success == 1) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
--- checkBitSize.h
+++ checkBitSize.h
@@ -8,33 +8,33 @@
 #define BITS_64 8
 
 #if defined __GNUC__
-	#if defined __i386
-		#define TARGET_SIZE BITS_32
-		#define PLATFORM "i386 linux"
-	#elif defined __x86_64
-		#define TARGET_SIZE BITS_64
-		#define PLATFORM "x86_64 linux"
-	#elif defined __arm__
-		#define TARGET_SIZE BITS_32
-		#define PLATFORM "arm linux"
-	#else
-		#define PLATFORM "invalid"
-		#error "unsupported architecture"
-	#endif
+        #if defined __i386
+                #define TARGET_SIZE BITS_32
+                #define PLATFORM "i386 linux"
+        #elif defined __x86_64
+                #define TARGET_SIZE BITS_64
+                #define PLATFORM "x86_64 linux"
+        #elif defined __arm__
+                #define TARGET_SIZE BITS_32
+                #define PLATFORM "arm linux"
+        #else
+                #define PLATFORM "invalid"
+                #error "unsupported architecture"
+        #endif
 #elif  defined _MSC_VER  && _MSC_VER >= 1800
-	#if defined _WIN64 && !defined _M_ARM
-		#define TARGET_SIZE BITS_64
-		#define PLATFORM "win64"
-	#elif defined _WIN32 && !defined _M_ARM
-		#define TARGET_SIZE BITS_32
-		#define PLATFORM "win32"
-	#else
-		#define PLATFORM "invalid"
-		#error "unsupported architecture"
-	#endif
+        #if defined _WIN64 && !defined _M_ARM
+                #define TARGET_SIZE BITS_64
+                #define PLATFORM "win64"
+        #elif defined _WIN32 && !defined _M_ARM
+                #define TARGET_SIZE BITS_32
+                #define PLATFORM "win32"
+        #else
+                #define PLATFORM "invalid"
+                #error "unsupported architecture"
+        #endif
 #else
-	#define PLATFORM "invalid compiler"
-	#error "unsupported compiler"
+        #define PLATFORM "invalid compiler"
+        #error "unsupported compiler"
 #endif
 
 #if defined _MSC_VER  && _MSC_VER <= 1800 /* MSVC */
@@ -51,7 +51,7 @@
 #define SIZE_T_FORMAT_STRING "%" SIZE_T_FORMAT
 
 #if defined _DEBUG  && defined _MSC_VER
-	#define MSVC_DEBUG_PAUSE system("pause")
+        #define MSVC_DEBUG_PAUSE system("pause")
 #else
-	#define MSVC_DEBUG_PAUSE 
-#endif 
+        #define MSVC_DEBUG_PAUSE
+#endif
